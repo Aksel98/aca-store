d4363c48eddc3e87f3a280f1b4ba9e3e9165d775
Armensch11 <armenschk@gmail.com>
2020-07-28 13:39:41 +0400

    added a tag

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Online Shop</h1>
+      <input type="text" />
+      <h1>Online Shop</h1>
     </div>
   );
 }
