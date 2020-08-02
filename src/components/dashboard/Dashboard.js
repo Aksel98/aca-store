931bb09ba557f605d17e2215c68124bf7c319bb1
aksel avetisyan <hello.dotoday@gmail.com>
2020-08-02 16:42:56 +0400

    change carousel component name

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Carousel from "./HeaderCarousel";
+import Carousel from "./Carousel";
 
 export default function Dashboard() {
     return (
