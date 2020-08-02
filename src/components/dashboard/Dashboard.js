578151dd8ee212ae952c815cc23a1de9853003d5
Vigen Tarkhanyan <vtarkhanyan@gmail.com>
2020-08-02 16:25:16 +0400

    create footer component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Carousel from "./Header-carousel";
+import Footer from "./Footer";
 
 export default function Dashboard() {
     return (
         <div>
             <Header />
             <Carousel />
+            <Footer />
         </div>
     )
 }
