4a8fb9f7d1b00d15ca06f4bdf4aae605651a06b8
Armensch11 <armenschk@gmail.com>
2020-08-12 15:46:57 +0400

    dont know

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Dashboard from "./dashboard/dashboard/Dashboard";
-<<<<<<< HEAD
 import { auth } from "./services/firebase/Firebase";
 import { UserContext } from "./main/context/UserContext";
 import { BasketContext } from "./main/context/BasketContext";
-
-export default function Main() {
-    const [currentUser, setCurrentUser] = useState(null)
-    const [basket, setBasket] = useState(JSON.parse(localStorage.getItem('ItemsInBasket')) || []);
-=======
-import {auth} from "./services/firebase/Firebase";
-import {UserContext} from "./main/context/UserContext";
 import Loader from "./main/Loader";
 
 export default function Main() {
-    const [currentUser, setCurrentUser] = useState('')
->>>>>>> bc2d803748240a0d57084bd8f053ea907dba662e
+
+    const [basket, setBasket] = useState(JSON.parse(localStorage.getItem('ItemsInBasket')) || []);
+    const [currentUser, setCurrentUser] = useState('');
 
     useEffect(() => {
         auth.onAuthStateChanged(user => {
@@ -25,13 +18,11 @@ export default function Main() {
 
 
     return (
-        currentUser === '' ? <Loader/> :
-        <UserContext.Provider value={currentUser}>
-            <BasketContext.Provider value={[basket, setBasket]}>
-                <Dashboard />
-            </BasketContext.Provider>
-
-
-        </UserContext.Provider>
+        currentUser === '' ? <Loader /> :
+            <UserContext.Provider value={currentUser}>
+                <BasketContext.Provider value={[basket, setBasket]}>
+                    <Dashboard />
+                </BasketContext.Provider>
+            </UserContext.Provider>
     )
 }
--- src/components/main/constants/Constants.js
+++ src/components/main/constants/Constants.js
@@ -9,14 +9,11 @@ export const ORANGE = '#cf8600'
 export const BLUE_GRADIENT = 'linear-gradient(to right, #02bbec, #0642ba) no-repeat 0 0 / cover'
 export const BLUE = '#3f51b5'
 export const GREY = 'rgb(197, 197, 197)';
-<<<<<<< HEAD
 export const LIGHTGREY = 'rgba(228, 228, 228, 0.7)';
 export const PURPLE = '#94;2FB8';
 export const RED = '#8B0000'
-export const BLUE = '#416284'
-=======
-export const PURPLE = '#942FB8';
->>>>>>> bc2d803748240a0d57084bd8f053ea907dba662e
+
+
 
 export const MyButton = styled(Button)({
     width: '100%',
